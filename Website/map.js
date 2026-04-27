e191af0b0b88ba1a992a54184a5c384e564382f3
Throngled <263249640+TheThrongled@users.noreply.github.com>
2026-04-27 21:25:03 +1000

    modified:   Website/index.html new file:   Website/map.js
    feat:
     implement interactive course
     roadmap with SVG pathing and
      contextual popups

---
+++ Website/map.js
@@ -0,0 +1,49 @@
+const milestones = [
+    { id: 1, x: 14, y: 48, title: "Intro to Logic" },
+    { id: 2, x: 33, y: 37, title: "Variables 101" },
+    { id: 3, x: 31, y: 85, title: "Functions" },
+    { id: 4, x: 57, y: 64, title: "Iterations" },
+    { id: 5, x: 76, y: 79, title: "Objects" },
+    { id: 6, x: 87, y: 40, title: "Heuristics" }
+];
+
+const nodesLayer = document.getElementById('nodes-layer');
+const svg = document.getElementById('mapSvg');
+
+// draw line and circle
+milestones.forEach((m, i) => {
+    // "makes" a circle
+    const node = document.createElement('div');
+    node.className = 'circle-node';
+    node.style.left = m.x + '%';
+    node.style.top = m.y + '%';
+    node.onclick = () => openPopup(m);
+    nodesLayer.appendChild(node);
+
+    // line "next" to circle
+    if (milestones[i + 1]) {
+        const next = milestones[i + 1];
+        const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        line.setAttribute("x1", m.x + "%");
+        line.setAttribute("y1", m.y + "%");
+        line.setAttribute("x2", next.x + "%");
+        line.setAttribute("y2", next.y + "%");
+        line.setAttribute("stroke", "#3498db");
+        line.setAttribute("stroke-width", "5");
+        svg.appendChild(line);
+    }
+});
+
+function openPopup(m) {
+    const popup = document.getElementById('map-popup');
+    
+    // popup adjusted to be centred relative to circle
+    popup.style.left = m.x + '%';
+    popup.style.top = m.y + '%';
+    
+    // fill in the popup
+    document.getElementById('popup-title').innerText = m.title;
+    document.getElementById('start-quiz-link').href = m.quizURL;
+    
+    popup.style.display = 'block';
+}
